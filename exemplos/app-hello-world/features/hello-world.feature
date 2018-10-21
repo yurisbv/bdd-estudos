dc14ad096bee447968090f190f22206f514d51ac
Yuri Brandao <yurisbv@gmail.com>
2018-10-21 11:48:37 -0300

    Correção no problema das aspas

--- exemplos/app-hello-world/features/hello-world.feature
+++ exemplos/app-hello-world/features/hello-world.feature
@@ -1,8 +1,9 @@
 Funcionalidade: Hello World na API
 
-Cenário: Mensagem 'Hello World'
-Dado a api no endereço: 'http://localhost:5000'
-Quando eu realizo um 'GET' no endpoint: '/'
-Então devo receber status code igual a '200'
-Então devo receber content-type igual a 'application/json'
-Então devo receber um body com '{"message":"Hello World"}'
+Cenário: Mensagem "Hello World"
+Dado a api no endereço: http://localhost:5000
+Quando eu realizo um GET no endpoint: /
+Então devo receber status code igual a 200
+Então devo receber content-type igual a application/json
+Então devo receber um body com {"message":"Hello World"}
+
--- exemplos/app-hello-world/features/steps/hello-world.py
+++ exemplos/app-hello-world/features/steps/hello-world.py
@@ -9,25 +9,25 @@ def api_url(context, api_url):
 
 @when(u'eu realizo um {http_verb} no endpoint: {endpoint}')
 def endpoint(context, http_verb, endpoint):
-    if http_verb.replace("'", "") == 'GET':
-        context.response = requests.get(url=context.api_url.replace("'", "") + endpoint.replace("'", ""))
+    if http_verb == 'GET':
+        context.response = requests.get(url=context.api_url + endpoint)
     context.data = context.response.json()
     assert context.response
 
 
 @then(u'devo receber status code igual a {status_code}')
 def status_code(context, status_code):
-    assert context.response.status_code == int(status_code.replace("'", ""))
+    assert context.response.status_code == int(status_code)
 
 
 @then(u'devo receber content-type igual a {content_type}')
 def content_type(context, content_type):
 
-    assert context.response.headers['content-type'] == content_type.replace("'", "")
+    assert context.response.headers['content-type'] == content_type
 
 
 @then(u'devo receber um body com {response}')
 def response(context, response):
-    test_json = json.loads(response.replace("'", ""))
+    test_json = json.loads(response)
     response_json = context.response.json()
     assert response_json['message'] == test_json['message']
